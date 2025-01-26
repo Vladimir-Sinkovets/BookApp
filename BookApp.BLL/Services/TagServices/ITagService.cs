54352fe64214bfb0410f55085f44f233bb690093
Vladimir Sinkovets <v.sinkovets@syberry.com>
2025-01-26 15:00:48 +0300

    Add tagService

---
+++ BookApp.BLL/Services/TagServices/ITagService.cs
@@ -0,0 +1,13 @@
+﻿using BookApp.BLL.Models;
+using BookApp.DAL.Models;
+
+namespace BookApp.BLL.Services.TagServices
+{
+    public interface ITagService
+    {
+        Task<TagData> CreateTagAsync(TagData tag);
+        Task DeleteTagAsync(int id);
+        IEnumerable<TagData> GetAll();
+        TagData GetById(int id);
+    }
+}
---
+++ BookApp.BLL/Services/TagServices/TagService.cs
@@ -0,0 +1,57 @@
+﻿using BookApp.BLL.Exceptions;
+using BookApp.BLL.Models;
+using BookApp.DAL.Models;
+using BookApp.DAL.Repositories;
+
+namespace BookApp.BLL.Services.TagServices
+{
+    public class TagService(IUnitOfWork unitOfWork) : ITagService
+    {
+        public IEnumerable<TagData> GetAll()
+        {
+            return unitOfWork.TagsRepository.GetAll()
+                .Take(100)
+                .Select(t => new TagData()
+                {
+                    Id = t.Id,
+                    Name = t.Name,
+                });
+        }
+        public TagData GetById(int id)
+        {
+            var tagEntry = unitOfWork.TagsRepository
+                .FirstOrDefault(t => t.Id == id);
+
+            if (tagEntry == null)
+                throw new NotFoundException();
+
+            return new TagData()
+            {
+                Id = tagEntry.Id,
+                Name = tagEntry.Name,
+            };
+        }
+
+        public async Task<TagData> CreateTagAsync(TagData tag)
+        {
+            var tagEntry = new TagEntry()
+            {
+                Id = tag.Id,
+                Name = tag.Name,
+            };
+
+            unitOfWork.TagsRepository.Add(tagEntry);
+
+            await unitOfWork.SaveChangesAsync(new CancellationToken());
+
+            return tag;
+        }
+
+        public async Task DeleteTagAsync(int id)
+        {
+            unitOfWork.TagsRepository.Remove(t => t.Id == id);
+
+            await unitOfWork.SaveChangesAsync(new CancellationToken());
+        }
+    }
+}
