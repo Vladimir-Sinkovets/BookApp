a38555a57afab719357b7929847831a501b4e386
Vladimir Sinkovets <v.sinkovets@syberry.com>
2025-01-29 13:58:44 +0300

    Rework routes

--- bookapp.client/src/app/app-routing.module.ts
+++ bookapp.client/src/app/app-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './books/pages/books/books.component';
 import { BookComponent } from './books/pages/book/book.component';
+import { AddBookComponent } from './books/pages/add-book/add-book.component';
 
 const routes: Routes = [
   {
     path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
   {
-    path: 'books-list', component: BooksComponent,
+    path: 'book/list', component: BooksComponent,
   },
   {
     path: 'book', component: BookComponent,
+  },
+  {
+    path: 'book/add', component: AddBookComponent,
   }
 ];
 
